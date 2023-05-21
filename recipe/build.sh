3cb63b6bc9886d2680961359e17b4ea4c616acae
Simon Li <orpheus+devel@gmail.com>
2023-05-21 19:55:19 +0100

    build.sh: support cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-export GOBIN="$PREFIX/bin"
+set -eux
 
-# https://github.com/stern/stern/blob/v1.21.0/.goreleaser.yaml#L7-L9
+# https://github.com/stern/stern/blob/v1.25.0/.goreleaser.yaml#L7-L9
 LDFLAGS="-X github.com/stern/stern/cmd.version=$PKG_VERSION-$PKG_BUILDNUM"
 
-go install -v -ldflags="$LDFLAGS" .
+go build -v -ldflags="$LDFLAGS" -o $PREFIX/bin/stern .
+
+# Cross-compilation: check correct architecture was built
+declare -A expected
+expected["darwin amd64"]='Mach-O.+x86_64'
+expected["darwin arm64"]='Mach-O.+arm64'
+expected["linux amd64"]='ELF.+x86-64'
+expected["linux arm64"]='ELF.+aarch64'
+if [ -n "$GOARCH" ]; then
+  file $PREFIX/bin/stern | grep -E "${expected["$GOOS $GOARCH"]}"
+fi
 
 go-licenses save . --save_path=./license-files
 test -d license-files/github.com
